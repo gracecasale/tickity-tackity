d57c47e7bf5d45ec205d3538b49aa7a45eccab93
Grace Casale <gracecasale@gmail.com>
2018-10-24 20:19:34 -0400

    changed html from individual rows to one large array

--- app.js
+++ app.js
@@ -5,38 +5,63 @@ const turnDisplay = document.querySelector('.turn span');
 const controls = document.querySelector('.controls');
 const resetBtn = controls.querySelector('button');
 let currentPlayer = 'X';
-let gameOver = false; 
+let gameOver = false;
+let grid = [
+    Cell(`<div class="cell h30 w30 bl-none bt-none"></div>`),
+    Cell(`<div class="cell h30 w30 bt-none"></div>`),
+    Cell(`<div class="cell h30 w30 bt-none br-none"></div>`),
+    Cell(`<div class="cell h30 w30 bl-none"></div>`),
+    Cell(`<div class="cell h30 w30"></div>`),
+    Cell(`<div class="cell h30 w30 br-none"></div>`),
+    Cell(`<div class="cell h30 w30 bl-none bb-none"></div>`),
+    Cell(`<div class="cell h30 w30 bb-none"></div>`),
+    Cell(`<div class="cell h30 w30 bb-none br-none"></div>`)
+    ];
+
+function Cell(template) {
+    const data = {
+        template: template,
+        hasBeenClicked: false
+    };
+    return data;
+}
 
 //events
-cells.forEach(function attachCellListener(cell){
-    cell.addEventListener('click', onCellClick); 
-}); 
+cells.forEach(function attachCellListener(cell) {
+    cell.addEventListener('click', onCellClick);
+});
 
-resetBtn.addEventListener('click', reset); 
+resetBtn.addEventListener('click', reset);
 
 //functions
-function onCellClick(event){
+function onCellClick(event) {
     const element = event.target;
-    if(element.beenClicked) {
+    if (element.beenClicked) {
         return;
     }
     element.beenClicked = true;
-    element.textContent = currentPlayer; 
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X'; 
+    element.textContent = currentPlayer;
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
     renderTurn();
 }
 
 function reset(event) {
- // alternatively --> location.reload();
+    // alternatively --> location.reload();
     currentPlayer = 'X';
-    cells.forEach(function resetCell (cell){
-        delete cell.beenClicked; 
+    cells.forEach(function resetCell(cell) {
+        delete cell.beenClicked;
         cell.textContent = '';
     });
-} 
+}
 
-function renderTurn(){
-    turnDisplay.textContent = currentPlayer; 
+function renderTurn() {
+    turnDisplay.textContent = currentPlayer;
+}
+
+function render() {
+    board.innerHTML = grid.map(function(element) {
+        return element.template;
+    }).join('\n');
 }
 
 //kick it off 
